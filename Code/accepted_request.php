9a5de8e1259acc03417b26ed19bea33259140a7d
samir mrassi <mrassi.samir@yahoo.fr>
2023-08-23 20:50:48 +0200

    Add new conversation after accepting a request

--- Code/accepted_request.php
+++ Code/accepted_request.php
@@ -5,15 +5,16 @@ session_start();
 // if the "accept request"-button is clicked:
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $requestId = $_POST['popupRequestId'];
-      //$requestId = 65;
-      // Update query
+
       $stmt = $conn->prepare("UPDATE requests SET request_status = '1' WHERE id_request = ?");
       $stmt->bind_param("i", $requestId);
-      if ($stmt->execute()) {
-        echo "success";
-      } else {
-        echo "error";
-      }
+      $stmt->execute();
+
+
+      $responsibleId = $_SESSION['user_id'];
+      $stmt = $conn->prepare("INSERT INTO conversations (id_responsible, id_request) VALUES (?,?)");
+      $stmt->bind_param("ii", $responsibleId, $requestId);
+      $stmt->execute();
     }
 
 ?>
