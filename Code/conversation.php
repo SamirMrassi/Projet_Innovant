3b9059000040f3d7cbe0db8b4455614fbe1f5eea
samir mrassi <mrassi.samir@yahoo.fr>
2023-08-27 20:43:41 +0200

    Backend conversation page improved

--- Code/conversation.php
+++ Code/conversation.php
@@ -24,8 +24,15 @@
 		<div class="container">
 	        <div class="conversations">
 	        	<?php
-					$stmt = $conn->prepare("SELECT * FROM conversations, requests,users WHERE conversations.id_request = requests.id_request AND conversations.id_responsible = ? AND conversations.id_responsible = users.id_user");
-					$stmt->bind_param("i", $_SESSION['user_id'] );
+					$stmt = $conn->prepare("SELECT c.id_conversation, c.id_responsible, c.id_request
+											FROM conversations c
+											WHERE c.id_responsible = ?
+											OR c.id_request IN (
+											    SELECT r.id_request
+											    FROM requests r
+											    WHERE r.id_user = ?
+											) OR c.id_request IN (SELECT r.id_request FROM requests r WHERE r.id_role=?); ");
+					$stmt->bind_param("iii", $_SESSION['user_id'], $_SESSION['user_id'], $_SESSION['role_id'] );
 					$stmt->execute();
 					$conversation_list = $stmt->get_result();
 
@@ -36,6 +43,7 @@
 						echo '<div class="conversation_information"> ';
 						echo '<div class="invisible_div" style="display:none;"> ' . $conv["id_conversation"] . ' </div>';
 						echo '<div class="invisible_div_userID" style="display:none;">' . $_SESSION['user_id'] . '</div>';
+						echo '<div class="invisible_div_username" style="display:none;">' . $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] . '</div>';
 						echo '<div class="conversation_information_request_id">' . $conv["id_request"] . '</div>';
 						echo '<div class="conversation_information_name_sender"> Envoyé par: ' . $conv["firstname_user"] . ' ' . $conv['lastname_user'] .' </div>';
 						echo ' </div>';
@@ -61,10 +69,7 @@
 		                <input type="text" id="message-input" placeholder="Type your message...">
 		                <button id="send-button">Envoyer</button>
 		            </div>
-		            <div id="button-container" style="display: none;">
-					    <button class="centered-create-button">Créer un ticket</button>
-					    <button class="centered-close-button">Fermer la demande</button>
-					</div>
+					<button class="change-conv-status" title="La conversation disparaitra">Clore la demande</button>
 	            </div>
 	        </div>
 	    </div>
@@ -76,59 +81,78 @@
 				const messages = document.querySelector(".messages");
 				const inputArea = document.querySelector(".input-area");
 				const messageInput = document.getElementById("message-input");
+				const changeStatus = document.querySelector(".change-conv-status");
 				const sendButton = document.getElementById("send-button");
-				const buttonContainer = document.getElementById("button-container");
-				const createButton = document.querySelector(".centered-create-button");
-				const closeButton = document.querySelector(".centered-close-button");
+				let username = document.querySelector(".invisible_div_username").textContent;
+				let interlocuteur = null;
 				let selectedConversation = null;
 				
 				// Add event listener to every conversation-block at the left of the screen
 				const conversation_information = document.querySelectorAll('.conversation_information');
             	conversation_information.forEach(conversation => {
 					conversation.addEventListener('click', () => {
-						//Make the messages- and input-areas visible 
+						const thisUserID = parseInt(conversation.querySelector('.invisible_div_userID').textContent);
+						// Make the messages- and input-areas visible 
 						messages.style.display = "flex";
 						inputArea.style.display = "flex";
+ 
+						// Si l'utilisateur est celui qui a fait la demande, rendre visible le bouton 
+						// qui changera le status de la demande
+						
 
-						buttonContainer.style.display = "block";
+						// Préparer la donnée de la conversation (id) à envoyer au script php 
+						// afin de recevoir les messages de la conversation de la base des données
 						selectedConversation  = {
 	                      id: conversation.querySelector('.invisible_div').textContent,
 	                    };
 	                    messages.innerHTML = '';
 
+	                    // Envoyer les information via Ajax et enclencher l'affichage des messages après les avoir reçus du script php.
 	                    const xhr = new XMLHttpRequest();
 				        xhr.open('POST', 'fetch_messages.php', true);
 				        xhr.onreadystatechange = function() {
 				            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
 				                const received_messages = JSON.parse(xhr.responseText);
-				                const first_received_msg = received_messages[0];
-				                if(first_received_msg === null){ // is there a created ticket for this conversation ?
-				                	// make the create-ticket-button visible
-				                	createButton.style.color = "red";
-				                }
-
-				                received_messages.shift();
 				                received_messages.forEach((msg, index) => {
+				                	//If this user created the request, he should be able to change the status of the request to completed
+				                	if(msg.request_user_id === thisUserID){
+				                		changeStatus.style.display = "block";
+				                	}else{
+				                		changeStatus.style.display = "none";
+				                	}
+
+				                	const senderDiv = document.createElement('div');
+				                    senderDiv.classList.add('message-information');
+
 				                    const messageDiv = document.createElement('div');
 				                    messageDiv.classList.add('actual-message');
 				                    messageDiv.textContent = msg.message_text;
 
-				                    if (msg.id_sender  === parseInt(conversation.querySelector('.invisible_div_userID').textContent)) {
-				                    	console.log(msg.id_sender);
+				                    if (msg.id_sender  === thisUserID) {
+				                    	username = msg.sender_firstname + " " + msg.sender_lastname;
+								        senderDiv.classList.add('sender-message');
+				                    	senderDiv.textContent = username;
 								        messageDiv.classList.add('actual-message', 'sender-message');
 								    } else {
+								    	interlocuteur = msg.sender_firstname + " " + msg.sender_lastname;
+								    	senderDiv.classList.add('receiver-message');
+								        senderDiv.textContent = interlocuteur;
 								        messageDiv.classList.add('actual-message', 'receiver-message');
 								    }
+								    messages.appendChild(senderDiv);
 				                    messages.appendChild(messageDiv);
 				                });
 				            }
 				        };
-				        
 				        const data = new FormData();
 						data.append("conv-id", selectedConversation.id);
 				        xhr.send(data);
+
+				        //Add eventlistener for closing a request if this user has created it
+				        
 					});
             	});
+
 
 				// Add event listener to send button
 				sendButton.addEventListener("click", function() {
@@ -151,11 +175,17 @@
                     	};
                 		xhr.send(data);
 
+                		const senderDiv = document.createElement('div');
+				        senderDiv.classList.add('message-information');
+				        senderDiv.classList.add('sender-message');
+				        senderDiv.textContent = username;
+
                 		const messageDiv = document.createElement('div');
 	                    messageDiv.classList.add('actual-message');
 	                    messageDiv.textContent = messageText;
 					    messageDiv.classList.add('actual-message', 'sender-message');
 					    
+					    messages.appendChild(senderDiv);
 	                    messages.appendChild(messageDiv);
 						messageInput.value = "";
 					}
